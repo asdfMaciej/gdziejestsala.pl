749e77518f553beefe78aca6d11523b51e7f0b19
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2022-12-26 22:09:57 +0100

    Read database settings from the .env file, add .env.example

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# This file is executed after each docker-compose up
+
 # Install requirements in case they changed
 pip install -r requirements.txt
 
 # Run database upgrades.
-# We don't need to wait and sleep for the database to start,
-#   as launch order is set in docker-compose.yml
+# The database is guaranteed to start, as launch order is set correctly
 alembic upgrade head
