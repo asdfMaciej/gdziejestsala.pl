badd0690aba8f4fde7900162b8f9805be31a1560
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2022-12-26 21:19:04 +0100

    Switch the database from SQLite3 to Postgres 14
    Postgres 14 seems like the newest fully supported version in SQLAlchemy
    
    Prestart.sh is executed on every docker container launch
    I've used it to install pip requirements in case they changed

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,21 +2,24 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://usos_user:usos_password@db/usos_db"
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+"""
+SQLite3 setup:
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
 # SQLite3 doesn't enforce foreign keys by default
 def _fk_pragma_on_connect(dbapi_con, con_record):
     dbapi_con.execute("pragma foreign_keys=ON")
 
-
 event.listen(engine, "connect", _fk_pragma_on_connect)
-
+"""
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ backend/prestart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Let the DB start
+#sleep 10;
+# Run migrations
+#alembic upgrade head
+
+# Install requirements in case they changed
+pip install -r requirements.txt
