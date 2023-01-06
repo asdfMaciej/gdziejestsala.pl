cc780d51ef543a6a8200a5c905ec6895ca3969fc
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2023-01-06 22:59:19 +0100

    Load floor and point details from cached API response

--- frontend/src/views/FloorView.vue
+++ frontend/src/views/FloorView.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { useDataStore } from '../stores/data';
+import { computed } from 'vue';
+
+const dataStore = useDataStore();
 
 const route = useRoute();
-const floor_id = route.params.floor_id;
+const routeFloorId = route.params.floor_id;
+
+const floor = computed(() => dataStore.floors.find(floor => floor.id == routeFloorId));
+
+const loading = computed(() => dataStore.floors.length == 0);
+
 </script>
 
 <template>
     <div class="about">
-        <h1>floor #{{ floor_id }} details</h1>
+        <h1>floor #{{ routeFloorId }} details</h1>
+
+        <template v-if="floor">
+            {{ floor }}
+        </template>
+        <template v-else-if="loading">
+            Ładowanie...
+        </template>
+        <template v-else>
+            Nie znaleziono piętra!
+        </template>
+
     </div>
 </template>
   
--- frontend/src/views/PointView.vue
+++ frontend/src/views/PointView.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { useDataStore } from '../stores/data';
+import { computed } from 'vue';
+
+const dataStore = useDataStore();
 
 const route = useRoute();
-const point_id = route.params.point_id;
+const routePointId = route.params.point_id;
+
+const point = computed(() => dataStore.points.find(point => point.id == routePointId));
+const loading = computed(() => dataStore.points.length == 0);
 </script>
 
 <template>
     <div class="about">
-        <h1>point #{{ point_id }} details</h1>
+        <h1>point #{{ routePointId }} details</h1>
+
+        <template v-if="point">
+            {{ point }}
+        </template>
+        <template v-else-if="loading">
+            Ładowanie...
+        </template>
+        <template v-else>
+            Nie znaleziono miejsca!
+        </template>
     </div>
 </template>
   
