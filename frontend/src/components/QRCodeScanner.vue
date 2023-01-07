f0450a8ef468f08b22e94bf833d11651f93f2f6d
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2023-01-07 02:23:45 +0100

    Install qrcode-reader-vue3 for scanning QR codes
    It's a fork of vue-qrcode-reader which supports Vue 3

---
+++ frontend/src/components/QRCodeScanner.vue
@@ -0,0 +1,101 @@
+
+<script lang="ts">
+// @ts-ignore
+import { QrcodeStream } from 'qrcode-reader-vue3'
+export default {
+    components: { QrcodeStream },
+
+    data() {
+        return {
+            result: '',
+            error: '',
+            camera: 'rear',
+
+            noRearCamera: false,
+            noFrontCamera: false
+        }
+    },
+
+    methods: {
+        switchCamera() {
+            switch (this.camera) {
+                case 'front':
+                    this.camera = 'rear'
+                    break
+                case 'rear':
+                    this.camera = 'front'
+                    break
+            }
+        },
+
+        onDecode(result: any) {
+            this.result = result
+        },
+
+        async onInit(promise: any) {
+            try {
+                await promise
+            } catch (error: any) {
+                if (error.name === 'NotAllowedError') {
+                    this.error = "ERROR: you need to grant camera access permission"
+                } else if (error.name === 'NotFoundError') {
+                    this.error = "ERROR: no camera on this device"
+                } else if (error.name === 'NotSupportedError') {
+                    this.error = "ERROR: secure context required (HTTPS, localhost)"
+                } else if (error.name === 'NotReadableError') {
+                    this.error = "ERROR: is the camera already in use?"
+                } else if (error.name === 'OverconstrainedError') {
+                    this.error = "ERROR: installed cameras are not suitable"
+                } else if (error.name === 'StreamApiNotSupportedError') {
+                    this.error = "ERROR: Stream API is not supported in this browser"
+                } else if (error.name === 'InsecureContextError') {
+                    this.error = 'ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.';
+                } else {
+                    this.error = `ERROR: Camera error (${error.name})`;
+                }
+
+                const triedFrontCamera = this.camera === 'front'
+                const triedRearCamera = this.camera === 'rear'
+
+                const cameraMissingError = error.name === 'OverconstrainedError'
+
+                if (triedRearCamera && cameraMissingError) {
+                    this.noRearCamera = true
+                }
+
+                if (triedFrontCamera && cameraMissingError) {
+                    this.noFrontCamera = true
+                }
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <p class="error">{{ error }}</p>
+
+        <p class="decode-result">Last result: <b>{{ result }}</b></p>
+
+        <qrcode-stream :camera="camera" @decode="onDecode" @init="onInit">
+            <button @click="switchCamera">
+                <img src="/assets/camera-switch.svg" alt="switch camera"> <!-- TODO: proper assets link -->
+            </button>
+        </qrcode-stream>
+    </div>
+</template>
+
+
+<style scoped>
+.error {
+    font-weight: bold;
+    color: red;
+}
+
+button {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import QRCodeScanner from '@/components/QRCodeScanner.vue';
 </script>
 
 <template>
   <main>
     <h2>Nie możesz znaleźć sali?</h2>
     <p>Zeskanuj nabliższy kod QR</p>
+
+    <QRCodeScanner></QRCodeScanner>
 
     <RouterLink :to="{ name: 'select-start', params: {} }">Lub zaznacz gdzie jesteś na liście</RouterLink>
   </main>
