13b3f7557be80b51e5365840e836fe69b8445a46
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2023-01-27 18:52:29 +0100

    Include psycopg2 in pigar-generated requirements.txt

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2  # Unused import - required for pigar
 
 from os import getenv
 
