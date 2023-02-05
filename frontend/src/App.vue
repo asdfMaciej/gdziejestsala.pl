358cd4dd92538d33545d9f758d7c88876fb476f5
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2023-02-05 18:40:27 +0100

    Create initial design for homepage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,28 +12,45 @@ const $debug = inject('$debug');
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav v-if="$debug">
-        <span>Debug menu (CD test):</span>
-        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-        <RouterLink :to="{ name: 'select-start', params: {} }">Select start</RouterLink>
-        <RouterLink :to="{ name: 'select-destination', params: { 'start_id': '1' } }">Select destination</RouterLink>
-        <RouterLink :to="{ name: 'view-route', params: { 'start_id': '1', 'destination_id': '2' } }">View route
-        </RouterLink>
-        <RouterLink :to="{ name: 'view-point', params: { 'point_id': '1' } }">Point details</RouterLink>
-        <RouterLink :to="{ name: 'view-floor', params: { 'floor_id': '1' } }">Floor details</RouterLink>
-        <RouterLink :to="{ name: 'about' }">About application</RouterLink>
-      </nav>
-    </div>
+  <header class="header">
+    <nav v-if="$debug">
+      <span>Debug menu (CD test):</span>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'select-start', params: {} }">Select start</RouterLink>
+      <RouterLink :to="{ name: 'select-destination', params: { 'start_id': '1' } }">Select destination</RouterLink>
+      <RouterLink :to="{ name: 'view-route', params: { 'start_id': '1', 'destination_id': '2' } }">View route
+      </RouterLink>
+      <RouterLink :to="{ name: 'view-point', params: { 'point_id': '1' } }">Point details</RouterLink>
+      <RouterLink :to="{ name: 'view-floor', params: { 'floor_id': '1' } }">Floor details</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About application</RouterLink>
+    </nav>
+    <span class="header__logo">gdziejestsala.pl</span>
+    <nav class="header__nav">
+
+    </nav>
   </header>
-  <RouterView v-slot="{ Component }">
-    <keep-alive>
-      <component :is="Component" :key="$route.fullPath"></component>
-    </keep-alive>
-  </RouterView>
+  <main>
+    <RouterView v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" :key="$route.fullPath"></component>
+      </keep-alive>
+    </RouterView>
+  </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.header {
+  background: var(--hex-key);
+  color: var(--hex-white);
+  padding: 20px var(--px-container-padding);
 
+  &__logo {
+    font-weight: bold;
+  }
+}
+
+main {
+  padding: 20px var(--px-container-padding);
+  flex: 1;
+}
 </style>
